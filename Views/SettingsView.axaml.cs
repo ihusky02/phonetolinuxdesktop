3ff715b204611e9cbca773c44ba47d9308330f13
ihusky02 <stanislawtlolka@gmail.com>
2026-09-17 04:57:51 +0200

    Add behind code to self update project

---
+++ Views/SettingsView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace phonetolinux.Views;
+
+public partial class SettingsView : UserControl
+{
+    public SettingsView()
+    {
+        InitializeComponent();
+    }
+}
